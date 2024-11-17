c9281995caa67cae4f7dcb617c3d3c6b274314e2
Samuel <samucadev54@gmail.com>
2024-11-17 16:38:49 -0300

    Removendo os arquivos duplicados fora da pasta 'portfolio'

--- Script.js
+++
@@ -1,9 +0,0 @@
-const themeToggle = document.getElementById('theme');
-
-themeToggle.addEventListener('change', () => {
-    if (themeToggle.checked) {
-        document.body.classList.add('dark');
-    } else {
-        document.body.classList.remove('dark');
-    }
-});
